a6bd775e0e71023c04ad5d6854f350284a72aa10
rahulraut11 <rahulraut110506@gmail.com>
2026-02-11 00:08:44 +0530

    Add bit manipulation macros

--- main.c
+++ main.c
@@ -13,6 +13,8 @@ const U64 not_hg_file = 4557430888798830399ULL ;
 #define get_bit(bitboard, square) (bitboard & (1ULL << square))
 #define set_bit(bitboard, square) (bitboard |=(1ULL << square)) 
 #define pop_bit(bitboard, square) (get_bit(bitboard,square) ? bitboard ^= (1ULL << square) : 0)
+#define count_bits(bitboard) __builtin_popcountll(bitboard)
+#define get_lsb_index(bb) __builtin_ctzll(bb)
 
 enum{
     a8, b8, c8, d8, e8, f8, g8, h8, 
@@ -29,7 +31,7 @@ enum{
     white, black
     };
 
-/*
+const char *square_to_cords[] = { 
  "a8" , "b8", "c8", "d8", "e8", "f8", "g8", "h8", 
  "a7" , "b7", "c7", "d7", "e7", "f7", "g7", "h7", 
  "a6" , "b6", "c6", "d6", "e6", "f6", "g6", "h6", 
@@ -38,7 +40,7 @@ enum{
  "a3" , "b3", "c3", "d3", "e3", "f3", "g3", "h3", 
  "a2" , "b2", "c2", "d2", "e2", "f2", "g2", "h2", 
  "a1" , "b1", "c1", "d1", "e1", "f1", "g1", "h1" 
-*/
+};
 
 // Attacks 
 
@@ -231,6 +233,6 @@ void print_board(U64 bitboard)
 // Main 
 int main()
 {
-    
+   
     return 0;
 }
