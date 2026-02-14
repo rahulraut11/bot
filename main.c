8fbee8934081cccbb561f915c5cdaa600aa316b3
rahulraut11 <rahulraut110506@gmail.com>
2026-02-14 12:17:23 +0530

    hardcoded magic numbers , initialized slider piece attack tables

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 // headers
 #include <stdio.h>
+#include <string.h>
 
 // definitions
 #define U64 unsigned long long
@@ -14,7 +15,8 @@ const U64 not_hg_file = 4557430888798830399ULL ;
 #define set_bit(bitboard, square) (bitboard |=(1ULL << square)) 
 #define pop_bit(bitboard, square) (get_bit(bitboard,square) ? bitboard ^= (1ULL << square) : 0)
 #define count_bits(bitboard) __builtin_popcountll(bitboard)
-#define get_lsb_index(bb) __builtin_ctzll(bb)
+#define get_lsb_index(bb) ((bb) ? __builtin_ctzll(bb) : -1)
+
 
 enum{
     a8, b8, c8, d8, e8, f8, g8, h8, 
@@ -27,9 +29,9 @@ enum{
     a1, b1, c1, d1, e1, f1, g1, h1 
 };
 
-enum{
-    white, black
-    };
+enum{ white, black };
+
+enum{ rook, bishop };
 
 const char *square_to_cords[] = { 
  "a8" , "b8", "c8", "d8", "e8", "f8", "g8", "h8", 
@@ -42,18 +44,154 @@ const char *square_to_cords[] = {
  "a1" , "b1", "c1", "d1", "e1", "f1", "g1", "h1" 
 };
 
+// rook magic numbers
+U64 rook_magic_numbers[64] = {
+    0x8a80104000800020ULL,
+    0x140002000100040ULL,
+    0x2801880a0017001ULL,
+    0x100081001000420ULL,
+    0x200020010080420ULL,
+    0x3001c0002010008ULL,
+    0x8480008002000100ULL,
+    0x2080088004402900ULL,
+    0x800098204000ULL,
+    0x2024401000200040ULL,
+    0x100802000801000ULL,
+    0x120800800801000ULL,
+    0x208808088000400ULL,
+    0x2802200800400ULL,
+    0x2200800100020080ULL,
+    0x801000060821100ULL,
+    0x80044006422000ULL,
+    0x100808020004000ULL,
+    0x12108a0010204200ULL,
+    0x140848010000802ULL,
+    0x481828014002800ULL,
+    0x8094004002004100ULL,
+    0x4010040010010802ULL,
+    0x20008806104ULL,
+    0x100400080208000ULL,
+    0x2040002120081000ULL,
+    0x21200680100081ULL,
+    0x20100080080080ULL,
+    0x2000a00200410ULL,
+    0x20080800400ULL,
+    0x80088400100102ULL,
+    0x80004600042881ULL,
+    0x4040008040800020ULL,
+    0x440003000200801ULL,
+    0x4200011004500ULL,
+    0x188020010100100ULL,
+    0x14800401802800ULL,
+    0x2080040080800200ULL,
+    0x124080204001001ULL,
+    0x200046502000484ULL,
+    0x480400080088020ULL,
+    0x1000422010034000ULL,
+    0x30200100110040ULL,
+    0x100021010009ULL,
+    0x2002080100110004ULL,
+    0x202008004008002ULL,
+    0x20020004010100ULL,
+    0x2048440040820001ULL,
+    0x101002200408200ULL,
+    0x40802000401080ULL,
+    0x4008142004410100ULL,
+    0x2060820c0120200ULL,
+    0x1001004080100ULL,
+    0x20c020080040080ULL,
+    0x2935610830022400ULL,
+    0x44440041009200ULL,
+    0x280001040802101ULL,
+    0x2100190040002085ULL,
+    0x80c0084100102001ULL,
+    0x4024081001000421ULL,
+    0x20030a0244872ULL,
+    0x12001008414402ULL,
+    0x2006104900a0804ULL,
+    0x1004081002402ULL
+};
+
+// bishop magic numbers
+U64 bishop_magic_numbers[64] = {
+    0x40040844404084ULL,
+    0x2004208a004208ULL,
+    0x10190041080202ULL,
+    0x108060845042010ULL,
+    0x581104180800210ULL,
+    0x2112080446200010ULL,
+    0x1080820820060210ULL,
+    0x3c0808410220200ULL,
+    0x4050404440404ULL,
+    0x21001420088ULL,
+    0x24d0080801082102ULL,
+    0x1020a0a020400ULL,
+    0x40308200402ULL,
+    0x4011002100800ULL,
+    0x401484104104005ULL,
+    0x801010402020200ULL,
+    0x400210c3880100ULL,
+    0x404022024108200ULL,
+    0x810018200204102ULL,
+    0x4002801a02003ULL,
+    0x85040820080400ULL,
+    0x810102c808880400ULL,
+    0xe900410884800ULL,
+    0x8002020480840102ULL,
+    0x220200865090201ULL,
+    0x2010100a02021202ULL,
+    0x152048408022401ULL,
+    0x20080002081110ULL,
+    0x4001001021004000ULL,
+    0x800040400a011002ULL,
+    0xe4004081011002ULL,
+    0x1c004001012080ULL,
+    0x8004200962a00220ULL,
+    0x8422100208500202ULL,
+    0x2000402200300c08ULL,
+    0x8646020080080080ULL,
+    0x80020a0200100808ULL,
+    0x2010004880111000ULL,
+    0x623000a080011400ULL,
+    0x42008c0340209202ULL,
+    0x209188240001000ULL,
+    0x400408a884001800ULL,
+    0x110400a6080400ULL,
+    0x1840060a44020800ULL,
+    0x90080104000041ULL,
+    0x201011000808101ULL,
+    0x1a2208080504f080ULL,
+    0x8012020600211212ULL,
+    0x500861011240000ULL,
+    0x180806108200800ULL,
+    0x4000020e01040044ULL,
+    0x300000261044000aULL,
+    0x802241102020002ULL,
+    0x20906061210001ULL,
+    0x5a84841004010310ULL,
+    0x4010801011c04ULL,
+    0xa010109502200ULL,
+    0x4a02012000ULL,
+    0x500201010098b028ULL,
+    0x8040002811040900ULL,
+    0x28000010020204ULL,
+    0x6000020202d0240ULL,
+    0x8918844842082200ULL,
+    0x4010011029020020ULL
+};
+
 // RNG
 
 // pseudo random number state
-unsigned int state = 1804289383;
+unsigned int random_state = 1804289383;
 // generate 32-bit pseudo legal numbers
 unsigned int get_random_U32_number()
 {
-    unsigned int number = state;
+    unsigned int number = random_state;
     number ^= number << 13;
     number ^= number >> 17;
     number ^= number << 5;
-    state = number;
+    random_state = number;
     return number;
 }
 
@@ -63,10 +201,10 @@ U64 get_random_U64_number()
     U64 n1, n2, n3, n4;
     
     // init random numbers slicing 16 bits from MS1B side
-    n1 = (U64)(get_random_U32_number() & 0xFFFF);
-    n2 = (U64)(get_random_U32_number() & 0xFFFF);
-    n3 = (U64)(get_random_U32_number() & 0xFFFF);
-    n4 = (U64)(get_random_U32_number() & 0xFFFF);
+    n1 = (U64)(get_random_U32_number()) & 0xFFFF;
+    n2 = (U64)(get_random_U32_number()) & 0xFFFF;
+    n3 = (U64)(get_random_U32_number()) & 0xFFFF;
+    n4 = (U64)(get_random_U32_number()) & 0xFFFF;
     
     // return random number
     return n1 | (n2 << 16) | (n3 << 32) | (n4 << 48);
@@ -86,7 +224,6 @@ U64 xorshift64star()
     rng_state = x;
     return x * 2685821657736338717ULL;
 }
-
 
 // Attacks 
 
@@ -120,6 +257,15 @@ U64 pawn_attacks[2][64] ;
 U64 knight_attacks[64] ;
 // king attacks table 
 U64 king_attacks[64] ;  
+// bishop attack masks
+U64 bishop_masks[64];
+// rook attack masks
+U64 rook_masks[64];
+
+// bishop attacks table [square][occupancies]
+U64 bishop_attacks[64][512];
+// rook attacks rable [square][occupancies]
+U64 rook_attacks[64][4096];
 
 // generate pawn attacks 
 U64 mask_pawn_attacks(int side, int square) 
@@ -208,7 +354,7 @@ U64 mask_rook_attacks(int square){
     return attacks ;
 }
 // generate bishop attacks
-U64 bishop_attacks(U64 bitboard, int square){
+U64 bishop_attacks_on_fly(int square, U64 bitboard){
     U64 attacks = 0ULL ;
 
     int rank = square/8 , file = square%8 ;
@@ -236,7 +382,7 @@ U64 bishop_attacks(U64 bitboard, int square){
     return attacks ;
 }
 // generate rook attacks 
-U64 rook_attacks(U64 bitboard , int square){
+U64 rook_attacks_on_fly(int square, U64 bitboard){
     U64 attacks = 0ULL ;
 
     int r, f;
@@ -296,6 +442,69 @@ U64 set_occupancy(int index, int bits_in_mask, U64 attack_mask){
     return occupancy ;
 }
 
+// init slider piece's attack tables
+void init_sliders_attacks(int bishop)
+{
+    for (int square = 0; square < 64; square++)
+    {
+        bishop_masks[square] = mask_bishop_attacks(square);
+        rook_masks[square] = mask_rook_attacks(square);
+        
+        U64 attack_mask = bishop ? bishop_masks[square] : rook_masks[square];
+        
+        int relevant_bits_count = count_bits(attack_mask);
+        
+        int occupancy_indicies = (1 << relevant_bits_count);
+        
+        // loop over occupancy indicies
+        for (int index = 0; index < occupancy_indicies; index++)
+        {
+            U64 occupancy = set_occupancy(index, relevant_bits_count, attack_mask);
+            if (bishop)
+            {
+                int magic_index = (occupancy * bishop_magic_numbers[square]) >> (64 - bishop_relevant_bits[square]);   
+                bishop_attacks[square][magic_index] = bishop_attacks_on_fly(square, occupancy);
+            }
+            else
+            {
+                int magic_index = (occupancy * rook_magic_numbers[square]) >> (64 - rook_relevant_bits[square]);
+                rook_attacks[square][magic_index] = rook_attacks_on_fly(square, occupancy);
+            }
+        }
+    }
+}
+
+/* These are critical lookup functions used in the inner loop of the search.
+First, inline removes the overhead of the function call stack. 
+Since these functions are very small—just a few bitwise operations—calling them normally would be inefficient.
+Inlining substitutes the code directly, which is crucial when we are calculating millions of nodes per second.
+Second, static gives them internal linkage. This allows me to define them in a header file so they can be used
+by both the Move Generator and the Evaluation function without causing 'multiple definition' linker errors */
+
+// get bishop attacks
+static inline U64 get_bishop_attacks(int square, U64 occupancy)
+{
+    // get bishop attacks assuming current board occupancy
+    occupancy &= bishop_masks[square];
+    occupancy *= bishop_magic_numbers[square];
+    occupancy >>= 64 - bishop_relevant_bits[square];
+    
+    // return bishop attacks
+    return bishop_attacks[square][occupancy];
+}
+
+// get rook attacks
+static inline U64 get_rook_attacks(int square, U64 occupancy)
+{
+    // get bishop attacks assuming current board occupancy
+    occupancy &= rook_masks[square];
+    occupancy *= rook_magic_numbers[square];
+    occupancy >>= 64 - rook_relevant_bits[square];
+    
+    // return rook attacks
+    return rook_attacks[square][occupancy];
+}
+
 //print
 void print_board(U64 bitboard)
 {
@@ -317,9 +526,35 @@ void print_board(U64 bitboard)
     printf("Bitboard: %llu \n", bitboard);
 }
 
+void init_all()
+{
+    init_leapers_attacks();
+    init_sliders_attacks(bishop);
+    init_sliders_attacks(rook);
+}
+
 // Main 
 int main()
 {
+    init_all() ; 
+    U64 occupancy = 0ULL;
     
+    // set blocker pieces on board
+    set_bit(occupancy, c5);
+    set_bit(occupancy, f2);
+    set_bit(occupancy, g7);
+    set_bit(occupancy, b2);
+    set_bit(occupancy, g5);
+    set_bit(occupancy, e2);
+    set_bit(occupancy, e7);
+    
+    // print occupancies
+    print_board(occupancy);
+    
+    // print rook attacks
+    print_board(get_rook_attacks(e5, occupancy));
+    
+    // print bishop attacks
+    print_board(get_bishop_attacks(d4, occupancy));
     return 0;
 }
